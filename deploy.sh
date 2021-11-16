e34556a40b66d5ef08e91c300af17297ee55c86b
rk365040 <rknaidu08@gmail.com>
2021-11-16 12:25:41 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo " download war file "
+wget http://18.222.145.132:8081/nexus/service/local/repositories/java/content/ram/sparkjava-hello-world/16/sparkjava-hello-world-16.war
+
+echo "war file downloadedf succefully"
